56a8023d1cdd5519b05e495c0bda031583e1ea32
Magnus Lidström <malstrom72@gmail.com>
2025-08-23 14:35:27 +0200

    Add Windows equivalents for documentation and build scripts

--- docs/updateDocs.sh
+++ docs/updateDocs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e -o pipefail -u
 cd "$(dirname "$0")"
 pandoc -s -o "Makaron Documentation.html" --metadata title="Makaron Documentation" --include-in-header pandoc.css "Makaron Documentation.md"
--- tools/buildMakaronCmd.command
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-chmod +x ./BuildCpp.sh
-./BuildCpp.sh release 64 ./makaron -I ../src/ ./MakaronCmd.cpp ../src/Makaron.cpp
