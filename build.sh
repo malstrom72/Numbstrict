077f243b670dff80e04ee0540bf3ee4d8fed2d58
Magnus Lidström <malstrom72@gmail.com>
2025-08-26 17:02:48 +0200

    Test round-trip float/double conversions

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ for target in beta release; do
 	CPP_OPTIONS="-std=c++11" bash tools/BuildCpp.sh "$target" native "$out_dir/smoke" \
 		-I src tests/smoke.cpp src/Numbstrict.cpp src/Makaron.cpp
 	"$out_dir/smoke" > /dev/null
+	CPP_OPTIONS="-std=c++11" bash tools/BuildCpp.sh "$target" native "$out_dir/doubleFloatToString" \
+		-I src tests/doubleFloatToString.cpp src/Numbstrict.cpp src/Makaron.cpp
+	"$out_dir/doubleFloatToString" > /dev/null
 	CPP_OPTIONS="-std=c++11" bash tools/BuildCpp.sh "$target" native "$out_dir/MakaronCmd" \
 		-I src tools/MakaronCmd.cpp src/Makaron.cpp
 done
